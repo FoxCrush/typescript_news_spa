cfe0dad65cbd8e5b53177303566cab38b932d20a
FoxCrush <foxteryr@gmail.com>
2023-01-16 00:51:32 +0200

    fixed readmore btn position

--- src/components/article-list-item-component/article-list-item.tsx
+++ src/components/article-list-item-component/article-list-item.tsx
@@ -1,8 +1,7 @@
 import { Card } from "./mui-styles-article-item";
-import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
-import { Button, CardActions } from "@mui/material";
+import { Box, Button } from "@mui/material";
 import { Link } from "react-router-dom";
 import CalendarIcon from "../../icons/calendar-icon";
 import ArrowIcon from "../../icons/arrow-icon";
@@ -31,7 +30,7 @@ export default function ArticleCard({ article }: Props) {
           image={article.imageUrl}
           alt={article.title}
         />
-        <CardContent sx={{ paddingTop: "25px" }}>
+        <Box sx={{ padding: "25px 20px 64px 20px" }}>
           <Typography
             gutterBottom
             variant="subtitle2"
@@ -47,21 +46,25 @@ export default function ArticleCard({ article }: Props) {
           <Typography variant="body2" color="text.secondary">
             {article.summary}
           </Typography>
-        </CardContent>
+        </Box>
       </Link>
-      <CardActions>
-        <Button
-          size="small"
-          color="primary"
-          onClick={() => {
-            clickHandler(article.id);
-          }}
-          sx={{ display: "flex" }}
-        >
-          <Link to={`/article/${article.id}`}>Read more</Link>
-          <ArrowIcon />
-        </Button>
-      </CardActions>
+
+      <Button
+        sx={{
+          display: "flex",
+          position: "absolute",
+          bottom: "25px",
+          left: "25px",
+        }}
+        size="small"
+        color="primary"
+        onClick={() => {
+          clickHandler(article.id);
+        }}
+      >
+        <Link to={`/article/${article.id}`}>Read more</Link>
+        <ArrowIcon />
+      </Button>
     </Card>
   );
 }
--- src/components/article-list-item-component/mui-styles-article-item.tsx
+++ src/components/article-list-item-component/mui-styles-article-item.tsx
@@ -1,6 +1,7 @@
 import { styled } from "@mui/material/styles";
 
 export const Card = styled("div")(({ theme }) => ({
+  position: "relative",
   margin: theme.spacing(3),
   flexBasis: "400px",
   minHeight: "530px",
@@ -12,4 +13,5 @@ export const Card = styled("div")(({ theme }) => ({
   "&:hover": {
     boxShadow: "0px 4px 4px 0px rgba(0, 0, 0, 0.25)",
   },
+  overflow: "hidden",
 }));
