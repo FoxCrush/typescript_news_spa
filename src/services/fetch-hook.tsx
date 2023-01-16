1a386009d8a2a44170f30db72997c927bdc5bd8c
FoxCrush <foxteryr@gmail.com>
2023-01-16 14:57:57 +0200

    debounce request

--- src/services/fetch-hook.tsx
+++ src/services/fetch-hook.tsx
@@ -1,21 +1,31 @@
 import axios from "axios";
 import { useState, useEffect, useMemo } from "react";
 import IArticle from "../interfaces/article-interface";
+import { debounce } from "lodash";
 
 axios.defaults.baseURL = "https://api.spaceflightnewsapi.net/v3/articles/";
-const limitResponseItemsPerRequest = 6; // restricted for development
+
+//const limitResponseItemsPerRequest = 6; restricted for development
 const useFetchFiltredArticles = (qString = "") => {
+  const [response, setResponse] = useState<IArticle[] | null>(null);
+  const [loading, setloading] = useState<boolean>(true);
   const params = useMemo(() => {
     return {
-      _limit: limitResponseItemsPerRequest,
       ...(qString.length > 0 && { title_contains: qString }),
       ...(qString.length > 0 && { summary_contains: qString }),
     };
   }, [qString]);
-  const [response, setResponse] = useState<IArticle[] | null>(null);
-  const [loading, setloading] = useState<boolean>(true);
 
+  const debouncedRequest = debounce(() => {
+    fetch();
+  }, 400);
   useEffect(() => {
+    return () => {
+      debouncedRequest.cancel();
+    };
+  }, [debouncedRequest]);
+
+  const fetch = () => {
     axios
       .get("", { params })
       .then((response) => {
@@ -27,7 +37,12 @@ const useFetchFiltredArticles = (qString = "") => {
       .finally(() => {
         setloading(false);
       });
-  }, [params, qString]);
+  };
+
+  useEffect(() => {
+    debouncedRequest();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [qString]);
 
   return { response, loading };
 };
